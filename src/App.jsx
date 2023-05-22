cebc48799fdf0fd8ce04785e70687ea7b2e4baf3
Melanie Santuccio <meli.santuccio@gmail.com>
2023-05-21 22:02:24 -0300

    Initial commit + layout

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { NavBarComponent } from "./components/NavBarComponent";
+
+function App() {
+  return (
+    <div className="main-container">
+      <NavBarComponent />
+      <main>probando main</main>
+      <footer className="main-title">probando footer</footer>
+    </div>
+  );
+}
+
+export default App;
